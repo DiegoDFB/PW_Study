fc15db2648ff29af684575e2a76a7016c66e3913
DiegoDFB <difrancisblanco@gmail.com>
2024-09-10 14:37:22 -0300

    a

--- src/main/java/etechoracio/pw_study/repository/ConexaoRepository.java
+++
@@ -1,9 +0,0 @@
-//Arthur de Jesus Lima e Diego Francischette Blanco
-
-package etechoracio.pw_study.repository;
-
-import etechoracio.pw_study.entity.Conexao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConexaoRepository extends JpaRepository<Conexao, Long> {
-}
--- src/main/java/etechoracio/pw_study/repository/DisponibilidadeRepository.java
+++
@@ -1,9 +0,0 @@
-//Arthur de Jesus Lima e Diego Francischette Blanco
-
-package etechoracio.pw_study.repository;
-
-import etechoracio.pw_study.entity.Disponibilidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DisponibilidadeRepository extends JpaRepository<Disponibilidade, Long> {
-}
